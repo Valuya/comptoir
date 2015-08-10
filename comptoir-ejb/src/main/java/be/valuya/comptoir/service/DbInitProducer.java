c212c99d31433e86d5abd9f23d3b2b0e30311578
Yannick Majoros <yannick@valuya.be>
2015-08-10 21:28:09 +0200

    liquibase (still fails)

--- comptoir-ejb/src/main/java/be/valuya/comptoir/service/DbInitProducer.java
+++ comptoir-ejb/src/main/java/be/valuya/comptoir/service/DbInitProducer.java
@@ -1,36 +1,50 @@
 package be.valuya.comptoir.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.sql.DataSource;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
 /**
  *
  * A Simple CDI Producer to configure the CDI Liquibase integration
  *
  * @author Yannick Majoros <yannick@valuya.be>
  */
-//@Singleton
+@Singleton
+@Startup
 public class DbInitProducer {
-//
-//    @Resource
-//    private DataSource myDataSource;
-//
-//    @Produces
-//    @LiquibaseType
-//    public CDILiquibaseConfig createConfig() {
-//        CDILiquibaseConfig config = new CDILiquibaseConfig();
-//        config.setChangeLog("db/main_changelog.xml");
-//        return config;
-//    }
-//
-//    @Produces
-//    @LiquibaseType
-//    public DataSource createDataSource() throws SQLException {
-//        return myDataSource;
-//    }
-//
-//    @Produces
-//    @LiquibaseType
-//    public ResourceAccessor create() {
-//        ClassLoader classLoader = DbInitProducer.class.getClassLoader();
-//        return new ClassLoaderResourceAccessor(classLoader);
-//    }
+
+    private static final Logger LOG = Logger.getLogger(DbInitProducer.class.getName());
+
+    @Resource(name = "jdbc/comptoir")
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void run() {
+        try {
+            LOG.info("Starting liquibase...");
+            Connection connection = dataSource.getConnection();
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
+            ClassLoaderResourceAccessor classLoaderResourceAccessor = new ClassLoaderResourceAccessor();
+
+            Liquibase liquibase = new Liquibase("db.changelog.xml", classLoaderResourceAccessor, database);
+            liquibase.update("");
+            LOG.info("Liquibase done.");
+        } catch (SQLException | LiquibaseException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
 }
