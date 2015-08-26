3fb4416dd138b825aa864ec9431dad01d8ed3670
Charly Ghislain <charlyghislain@gmail.com>
2015-08-26 13:08:16 +0200

    /sale/totalPayed: allow closed sales

--- comptoir-ws/src/main/java/be/valuya/comptoir/ws/rest/control/SaleResource.java
+++ comptoir-ws/src/main/java/be/valuya/comptoir/ws/rest/control/SaleResource.java
@@ -141,7 +141,7 @@ public class SaleResource {
     @Path("{id}/payed")
     public BigDecimal getSaleTotalPayed(@PathParam("id") long id) {
         Sale sale = saleService.findSaleById(id);
-        saleStateChecker.checkState(sale, false); // TODO: replace with bean validation
+        //saleStateChecker.checkState(sale, false); // TODO: replace with bean validation
 
         return saleService.getSaleTotalPayed(sale);
     }
