5dbb3472684d6a8006735cd2ce32f54f3f137cf6
Yannick Majoros <yannick@valuya.be>
2015-08-26 14:02:06 +0200

    @NoId works

--- comptoir-ws/src/main/java/be/valuya/comptoir/ws/rest/validation/NoId.java
+++ comptoir-ws/src/main/java/be/valuya/comptoir/ws/rest/validation/NoId.java
@@ -1,9 +1,26 @@
 package be.valuya.comptoir.ws.rest.validation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+
 /**
  *
  * @author Yannick Majoros <yannick@valuya.be>
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Documented
+@Constraint(validatedBy = NoIdValidator.class)
 public @interface NoId {
-    
+
+    String message() default "Object is expected to have no id for this operation.";
+
+    Class[] groups() default {};
+
+    Class[] payload() default {};
+
 }
