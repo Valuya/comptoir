a3207456f9d937191a9c0cac8cb53d6f32949286
Yannick Majoros <yannick@valuya.be>
2015-11-17 07:34:36 +0100

    minor refactoring

--- comptoir-ws/src/main/java/be/valuya/comptoir/ws/rest/filter/CrossOriginResourceSharingFilter.java
+++ comptoir-ws/src/main/java/be/valuya/comptoir/ws/rest/filter/CrossOriginResourceSharingFilter.java
@@ -4,6 +4,7 @@ import be.valuya.comptoir.ws.config.HeadersConfig;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -15,10 +16,11 @@ public class CrossOriginResourceSharingFilter implements ContainerResponseFilter
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) {
-        response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-        response.getHeaders().putSingle("Access-Control-Allow-Headers", "content-type, accept, accept-charset, authorisation");
-        response.getHeaders().putSingle("Access-Control-Expose-Headers", HeadersConfig.LIST_RESULTS_COUNT_HEADER);
+        MultivaluedMap<String, Object> headerMap = response.getHeaders();
+        headerMap.putSingle("Access-Control-Allow-Origin", "*");
+        headerMap.putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+        headerMap.putSingle("Access-Control-Allow-Headers", "content-type, accept, accept-charset, authorisation");
+        headerMap.putSingle("Access-Control-Expose-Headers", HeadersConfig.LIST_RESULTS_COUNT_HEADER);
     }
     
 }
